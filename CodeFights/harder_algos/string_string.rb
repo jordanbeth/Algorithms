e74272603ea85254f36a4d53fd8cf568d4a0ffcc
Jordan Beth <jordanbeth14@gmail.com>
2017-04-08 15:40:16 -0400

    completed strstr

---
+++ CodeFights/harder_algos/string_string.rb
@@ -0,0 +1,30 @@
+=begin
+Problem
+
+- Implement a function that takes two strings, s and x, as arguments and finds the first occurrence of the string x in s.
+
+- The function should return an integer indicating the index in s of the first occurrence of x.
+
+- If there are no occurrences of x in s, return -1.
+
+examples: 
+For s = "CodefightsIsAwesome" and x = "IA", the output should be
+strstr(s, x) = -1;
+For s = "CodefightsIsAwesome" and x = "IsA", the output should be
+strstr(s, x) = 10.
+=end
+
+def strstr(s, x)
+  x_length = x.length
+  s_array = s.split("")
+
+  s_array.each_with_index do |char, idx|
+    if (char == x[0])
+      search_me = s_array[idx...idx+x_length]
+      if search_me.join("") == x
+        return idx
+      end
+    end
+  end
+  return -1
+end
