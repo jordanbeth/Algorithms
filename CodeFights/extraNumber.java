2e75c45972ab8b2bcb1fa7c7e95c0f854ec5acad
Jordan Beth <jordanbeth14@gmail.com>
2017-04-09 13:41:53 -0400

    finished is inifinte proces

--- CodeFights/extraNumber.java
+++ CodeFights/extraNumber.java
@@ -1,22 +1,22 @@
 /*
-Problem Extra Number
+   Problem Extra Number
 
-- You're given three integers, a, b and c. It is guaranteed that two of these integers are equal to each other.
-- What is the value of the third integer?
+   - You're given three integers, a, b and c. It is guaranteed that two of these integers are equal to each other.
+   - What is the value of the third integer?
 
-Example
-* For a = 2, b = 7 and c = 2,
-  - output - :  extraNumber(a, b, c) = 7
-*/
+   Example
+ * For a = 2, b = 7 and c = 2,
+   - output - :  extraNumber(a, b, c) = 7
+ */
 
 int extraNumber(int a, int b, int c){
-  if (a == b){
-    return c;
-  }else if(b == c){
-    return a;
-  }else if (a == c){
-    return b;
-  }else{
-    return -1;
-  }
+        if (a == b) {
+                return c;
+        }else if(b == c) {
+                return a;
+        }else if (a == c) {
+                return b;
+        }else{
+                return -1;
+        }
 }
