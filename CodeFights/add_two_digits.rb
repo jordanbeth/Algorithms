53f86a4e7dea57957fc8f9e1f4693142578567a0
Jordan Beth <jordanbeth14@gmail.com>
2017-04-09 07:26:59 -0400

    starting add two digits problem

---
+++ CodeFights/add_two_digits.rb
@@ -0,0 +1,12 @@
+=begin
+Problem
+
+You are given a two-digit integer n. Return the sum of its digits.
+=end
+
+def addTwoDigts(n)
+  # n.to_s.split("").map(:to_i).reduce(:+)
+end
+
+
+p addTwoDigts(58)
