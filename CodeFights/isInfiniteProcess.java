606921b14c9df1fac2e7a2ddbe1b2c341daa321a
Jordan Beth <jordanbeth14@gmail.com>
2017-04-09 13:42:17 -0400

    beautify

--- CodeFights/isInfiniteProcess.java
+++ CodeFights/isInfiniteProcess.java
@@ -1,18 +1,18 @@
 /*
-Problem Is Infinite Process?
+   Problem Is Infinite Process?
 
-Given integers a and b, determine whether the following pseudocode results in an infinite loop
+   Given integers a and b, determine whether the following pseudocode results in an infinite loop
 
-while a is not equal to b do
-  increase a by 1
-  decrease b by 1
+   while a is not equal to b do
+   increase a by 1
+   decrease b by 1
 
-Assume that the program is executed on a virtual machine which can store arbitrary long numbers and execute forever.
-*/
+   Assume that the program is executed on a virtual machine which can store arbitrary long numbers and execute forever.
+ */
 
 boolean isInfiniteProcess(int a, int b){
-  if ((a > b) || ((b - a) % 2 == 1)){
-    return true;
-  }
-  return false;
+        if ((a > b) || ((b - a) % 2 == 1)) {
+                return true;
+        }
+        return false;
 }
