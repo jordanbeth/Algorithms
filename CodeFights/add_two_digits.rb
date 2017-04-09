4e670b3e505310484692bbd1ad79c7ded298960d
Jordan Beth <jordanbeth14@gmail.com>
2017-04-09 07:34:18 -0400

    finished add two digits

--- CodeFights/add_two_digits.rb
+++ CodeFights/add_two_digits.rb
@@ -4,9 +4,6 @@ Problem
 You are given a two-digit integer n. Return the sum of its digits.
 =end
 
-def addTwoDigts(n)
-  # n.to_s.split("").map(:to_i).reduce(:+)
+def addTwoDigits(n)
+  n.to_s.split("").map(&:to_i).reduce(:+)
 end
-
-
-p addTwoDigts(58)
